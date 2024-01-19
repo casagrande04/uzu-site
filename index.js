a01d51a19729cc2294f3f2251093d1e09039e5cf
Eduardo <edu.casagrande@hotmail.com>
2024-01-19 12:53:39 -0300

    Alterações modo claro

---
+++ index.js
@@ -0,0 +1,27 @@
+//LIGHT MODE/ DARK MODE
+var estiloAtual = 1;
+
+function trocarCSS() {
+    var estilo = document.getElementById('estilo');
+
+    // Troca entre os estilos ao clicar no botão
+    if (estiloAtual === 1) {
+        estilo.href = '../style/style.css';
+        estiloAtual = 2;
+        // trocar imagem claro / escuro
+        var img = document.querySelector("#cdg");
+        img.setAttribute('src', '/img/cdg.png');
+
+        var img = document.querySelector("#mode");
+        img.setAttribute('src', '/img/darkmode.png');
+    } else {
+        estilo.href = '../style/lightmode.css';
+        estiloAtual = 1;
+        // trocar imagem claro / escuro
+        var img = document.querySelector("#cdg");
+        img.setAttribute('src', '/img/cdg-light.png');
+
+        var img = document.querySelector("#mode");
+        img.setAttribute('src', '/img/lightmode.png');
+    }
+}
